fddb8b959ff5c577959e2f5047801645c14c48a4
LeCodeurDeParis <Pl.Baillion@gmail.com>
2024-04-30 11:41:17 +0200

    Ajout du Jump Buffering

--- SuperSquareLand-clean-version/Assets/SSL/Runtime/Scripts/Hero/HeroController.cs
+++ SuperSquareLand-clean-version/Assets/SSL/Runtime/Scripts/Hero/HeroController.cs
@@ -8,6 +8,10 @@ public class HeroController : MonoBehaviour
     [Header("Dash Settings")]
     [SerializeField] private HeroDashSettings _dashSettings;
 
+    [Header("Jump Buffer")]
+    [SerializeField] private float _jumpBufferDuration = 0.2f;
+    private float _jumpBufferTimer = 0f;
+
     [Header("Debug")]
     [SerializeField] private bool _guiDebug = false;
 
@@ -17,7 +21,13 @@ public class HeroController : MonoBehaviour
 
         GUILayout.BeginVertical(GUI.skin.box);
         GUILayout.Label(gameObject.name);
+        GUILayout.Label($"Jump Buffer Timer = {_jumpBufferTimer}");
         GUILayout.EndVertical();
+    }
+
+    private void Start()
+    {
+        _CancelJumpBuffer();
     }
 
     private bool _GetInputJump()
@@ -26,11 +36,21 @@ public class HeroController : MonoBehaviour
         }
     private void Update()
     {
+        _UpdateJumpBuffer();
+
         _entity.SetMoveDirX(GetInputMoveX());
 
-       
-
         if (_GetInputDownJump())
+        {
+            if (_entity.IsTouchingGround && !_entity.IsJumping)
+            {
+                _entity.JumpStart();
+            } else {
+                _ResetJumpBuffer();
+            }
+        }
+
+        if (IsJumpBufferActive())
         {
             if (_entity.IsTouchingGround && !_entity.IsJumping)
             {
@@ -66,5 +86,22 @@ public class HeroController : MonoBehaviour
     private bool _GetInputDownJump()
     {
         return Input.GetKeyDown(KeyCode.Space);
+    }
+
+    private void _ResetJumpBuffer(){
+        _jumpBufferTimer = 0f;
+    }
+
+    private bool IsJumpBufferActive(){
+        return _jumpBufferTimer < _jumpBufferDuration;
+    }
+
+    private void _UpdateJumpBuffer(){
+        if (!IsJumpBufferActive()) return;
+        _jumpBufferTimer += Time.deltaTime;
+    }
+
+    private void _CancelJumpBuffer(){
+        _jumpBufferTimer = _jumpBufferDuration;
     }
 }
